4d45365cad07b20f03c397666831cf58e7a9bd43
weidaomei <94629115+weidaomei@users.noreply.github.com>
2022-10-24 16:07:49 +0800

    test

---
+++ src/test/java/com/javacurriculumdesign/pvms/PvmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacurriculumdesign.pvms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
